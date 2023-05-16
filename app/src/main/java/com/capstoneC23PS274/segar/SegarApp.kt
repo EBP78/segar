5eb6fa992a33a3f54eb7aa3903fb05acce4f914b
EBP78 <edwardbrainp78@gmail.com>
2023-05-16 12:19:00 +0700

    first commit: android studio template and some depedencies edit

---
+++ app/src/main/java/com/capstoneC23PS274/segar/SegarApp.kt
@@ -0,0 +1,11 @@
+package com.capstoneC23PS274.segar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SegarApp(
+    modifier: Modifier = Modifier
+) {
+
+}
