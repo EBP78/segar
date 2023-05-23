561388164affb4d01a8eb8141a2a87d86d1ab0d7
“VallianDjong” <valldjong@gmail.com>
2023-05-23 13:36:47 +0700

    making register screen.

---
+++ app/src/main/java/com/capstoneC23PS274/segar/ui/screen/register/RegisterScreen.kt
@@ -0,0 +1,145 @@
+package com.capstoneC23PS274.segar.ui.screen.register
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+val MainGreen = Color(0xFF7DC09C)
+@Composable
+fun RegisterScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("https://cdn.pixabay.com/photo/2016/07/16/03/50/pigs-1520968_1280.jpg")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp)
+                    .padding(50.dp)
+            )
+        }
+        Column {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                border = BorderStroke(1.dp, MainGreen)
+            ) {
+                Text(
+                    text = "Username",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                border = BorderStroke(1.dp, MainGreen)
+            ) {
+                Text(
+                    text = "Email",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                border = BorderStroke(1.dp, MainGreen)
+            ) {
+                Text(
+                    text = "Password",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                border = BorderStroke(1.dp, MainGreen)
+            ) {
+                Text(
+                    text = "Confirm Password",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(430.dp)
+            )
+            Button(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(80.dp)
+                    .padding(8.dp),
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = MainGreen)
+            ) {
+                Text(
+                    text = "Register",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp,
+                )
+            }
+        }
+    }
+}
+    @Composable
+    @Preview(showBackground = true)
+    fun LoginPreview() {
+        MaterialTheme {
+            RegisterScreen(
+                modifier = Modifier
+            )
+        }
+    }
