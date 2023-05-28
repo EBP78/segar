331a4213b9ae557fa56dc2706795762b1a53688c
EBP78 <edwardbrainp78@gmail.com>
2023-05-28 15:19:24 +0700

    adding History Screen

---
+++ app/src/main/java/com/capstoneC23PS274/segar/ui/screen/history/HistoryScreen.kt
@@ -0,0 +1,19 @@
+package com.capstoneC23PS274.segar.ui.screen.history
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.capstoneC23PS274.segar.data.remote.response.HistoryResponseItem
+import com.capstoneC23PS274.segar.ui.component.HistoryListItem
+
+@Composable
+fun HistoryScreen(
+    modifier: Modifier = Modifier
+) {
+    // getting data from viewmodel
+    // example of history item use
+    val historyItem = HistoryResponseItem("001", "sawi", "https://cdn.pixabay.com/photo/2016/07/16/03/50/pigs-1520968_1280.jpg", "80% Seggar", "10 oktober 2023")
+    HistoryListItem(
+        itemData = historyItem,
+        modifier = Modifier
+    )
+}
