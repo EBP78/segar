471f99ac84d61720fb22596d58886f81a145ea28
EBP78 <edwardbrainp78@gmail.com>
2023-05-30 13:53:29 +0700

    adding screen object

--- app/src/main/java/com/capstoneC23PS274/segar/ui/navigation/Screen.kt
+++ app/src/main/java/com/capstoneC23PS274/segar/ui/navigation/Screen.kt
@@ -3,7 +3,16 @@ package com.capstoneC23PS274.segar.ui.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Dictionary : Screen("dictionary")
+    object DictionaryDetail : Screen("dictionary/{dictId}"){
+        fun createRoute(dictId: String) = "dictionary/$dictId"
+    }
     object Check : Screen("check")
+    object Result : Screen("check/{resultId}"){
+        fun createRoute(resultId: String) = "check/$resultId"
+    }
     object History : Screen("history")
     object Profile : Screen("profile")
+    object Faq : Screen("faq")
+    object Login : Screen("login")
+    object Register : Screen("register")
 }
