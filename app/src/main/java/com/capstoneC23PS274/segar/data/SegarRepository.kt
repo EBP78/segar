9ad529166d784024761628a38f8a477ee7b9adb3
EBP78 <edwardbrainp78@gmail.com>
2023-06-01 13:17:02 +0700

    making user preference for handle token and login status

---
+++ app/src/main/java/com/capstoneC23PS274/segar/data/SegarRepository.kt
@@ -0,0 +1,8 @@
+package com.capstoneC23PS274.segar.data
+
+import com.capstoneC23PS274.segar.data.preference.UserPreference
+import com.capstoneC23PS274.segar.data.remote.retrofit.ApiService
+
+class SegarRepository (private val apiService: ApiService, private val userPreference: UserPreference) {
+
+}
