7dc031349faa9c95795c71ba85b559d5905f4c2d
EBP78 <edwardbrainp78@gmail.com>
2023-06-01 12:46:08 +0700

    making apiconfig using retrofit

---
+++ app/src/main/java/com/capstoneC23PS274/segar/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,11 @@
+package com.capstoneC23PS274.segar.data.remote.retrofit
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.POST
+
+interface ApiService{
+    
+}
