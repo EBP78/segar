9d92125fb999743520e76d955aa488c7b8950a15
EBP78 <edwardbrainp78@gmail.com>
2023-06-10 12:09:02 +0700

    adding some testing

--- app/src/androidTest/java/com/capstoneC23PS274/segar/SegarAppTest.kt
+++ app/src/androidTest/java/com/capstoneC23PS274/segar/SegarAppTest.kt
@@ -6,12 +6,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.capstoneC23PS274.segar.ui.navigation.Screen
 import com.capstoneC23PS274.segar.ui.theme.SegarTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
@@ -37,8 +39,7 @@ class SegarAppTest {
 
     @Test
     fun splash_goToLogin_ifNotLoginYet_viceVersaToHome() = runTest {
-        val currentRoute = navController.currentBackStackEntry?.destination?.route
-        assertEquals(Screen.Splash.route, currentRoute)
+        navController.assertCurrentRouteName(Screen.Splash.route)
         delay(3000)
         if (navController.isTheSameRoute(Screen.Login.route)){
             navController.assertCurrentRouteName(Screen.Login.route)
@@ -68,15 +69,38 @@ class SegarAppTest {
 
     @Test
     fun home_isDisplayed() =  runTest {
-        val currentRoute = navController.currentBackStackEntry?.destination?.route
-                assertEquals(Screen.Splash.route, currentRoute)
-        delay(3000)
+        navController.assertCurrentRouteName(Screen.Splash.route)
+        composeTestRule.mainClock.advanceTimeBy(4000)
         if (navController.isTheSameRoute(Screen.Login.route)){
             navController.assertCurrentRouteName(Screen.Login.route)
-            
-        } else if (navController.isTheSameRoute(Screen.Home.route)) {
-            navController.assertCurrentRouteName(Screen.Home.route)
+            composeTestRule.onNodeWithTagStringId(R.string.email).performTextInput("testaj4@gmail.com")
+            composeTestRule.onNodeWithTagStringId(R.string.password).performTextInput("asd")
+            composeTestRule.onNodeWithStringId(R.string.login).performClick()
+            advanceUntilIdle()
+            delay(3000)
         }
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun all_bottomNavigationWorks() = runTest {
+        home_isDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.menu_dict).performClick()
+        navController.assertCurrentRouteName(Screen.Dictionary.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_history).performClick()
+        navController.assertCurrentRouteName(Screen.History.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_profile).performClick()
+        navController.assertCurrentRouteName(Screen.Profile.route)
+        composeTestRule.onNodeWithStringId(R.string.menu_home).performClick()
+        navController.assertCurrentRouteName(Screen.Home.route)
+    }
+
+    @Test
+    fun home_contentIsDisplayed() = runTest {
+        home_isDisplayed()
+        composeTestRule.onNodeWithContentDescStringId(R.string.app_logo).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescStringId(R.string.splash_image).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescStringId(R.string.tagline).assertIsDisplayed()
     }
 
 
--- app/src/androidTest/java/com/capstoneC23PS274/segar/Utils.kt
+++ app/src/androidTest/java/com/capstoneC23PS274/segar/Utils.kt
@@ -4,6 +4,8 @@ import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.NavController
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -20,3 +22,11 @@ fun NavController.isTheSameRoute(expectedRouteName: String) : Boolean {
 fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
     @StringRes id: Int,
 ) : SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithTagStringId(
+    @StringRes id: Int,
+) : SemanticsNodeInteraction = onNodeWithTag(activity.getString(id))
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithContentDescStringId(
+    @StringRes id: Int,
+) : SemanticsNodeInteraction = onNodeWithContentDescription(activity.getString(id))
